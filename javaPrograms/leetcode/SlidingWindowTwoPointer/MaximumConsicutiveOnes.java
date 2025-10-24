35d00835f29dfa8382f4e4e9fab30c6d9bf8f155
aameensiddiqui <aameensddq@gmail.com>
2025-10-25 00:19:45 +0530

    leetcode: maximum points you can obtain from cards

--- javaPrograms/leetcode/SlidingWindowTwoPointer/MaximumConsicutiveOnes.java
+++ javaPrograms/leetcode/SlidingWindowTwoPointer/MaximumConsicutiveOnes.java
@@ -23,13 +23,10 @@
 */
 class MaximumConsicutiveOnes {
     public static int longestOnes(int[]nums, int k) {
-        int r = 0, l = 0;
-        int maxLen = 0;
-        int zeros = 0;
-        int len = 0;
+        int r = 0, l = 0, len = 0, maxLen = 0, zeros = 0;
         while(r < nums.length) {
             if(nums[r] == 0) zeros++;
-            while(zeros > k) {
+            if(zeros > k) {
                 if(nums[l] == 0) zeros--;
                 l++;
             }
@@ -49,6 +46,27 @@ class MaximumConsicutiveOnes {
     }
     private static final Log log = new Log();
 }
+/**
+    public static int longestOnes(int[]nums, int k) {
+        int r = 0, l = 0;
+        int maxLen = 0;
+        int zeros = 0;
+        int len = 0;
+        while(r < nums.length) {
+            if(nums[r] == 0) zeros++;
+            while(zeros > k) {
+                if(nums[l] == 0) zeros--;
+                l++;
+            }
+            if(zeros <= k) {
+                len = r-l+1;
+                maxLen = Math.max(maxLen, len);
+            }
+            r++;
+        }
+        return maxLen;
+    }
+ */
 /**
 public static int longestOnes(int[]nums, int k) {
         int maxLen = 0;
