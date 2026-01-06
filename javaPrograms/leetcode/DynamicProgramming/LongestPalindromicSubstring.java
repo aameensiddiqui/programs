3acc2dec879793d003f5b57f3969535b2e887bf6
aameensiddiqui <aameensddq@gmail.com>
2026-01-06 17:12:28 +0530

    leetcode: longest palindromic substring

--- javaPrograms/leetcode/DynamicProgramming/LongestPalindromicSubstring.java
+++ javaPrograms/leetcode/DynamicProgramming/LongestPalindromicSubstring.java
@@ -18,24 +18,75 @@
 */
 class LongestPalndromicSubstring {
     private static String longestPalindrome(String s) {
-        int resLen = 0, resInd = 0, n = s.length();
+        int n = s.length(), resLen = 0, resIdx = 0;
         boolean[][] dp = new boolean[n][n];
+
         for (int i = n-1; i >= 0; --i) {
             for (int j = i; j < n; ++j) {
-                if (s.charAt(i) == s.charAt(j) &&
-                    (j-i <= 2 || dp[i+1][j-1])) {
+                if (s.charAt(i) == s.charAt(j) && (j-i <= 2 || dp[i+1][j-1])) {
                     dp[i][j] = true;
                     if (resLen < (j-i+1)) {
-                        resInd = i;
+                        resIdx = i;
                         resLen = j-i+1;
                     }
                 }
             }
         }
-        return s.substring(res, resLen+resInd);
+        return s.substring(resIdx, resLen+resIdx);
     }
     public static void main(String[]args) {
         log.info(longestPalindrome("babad"));
         log.info(longestPalindrome("cbbd"));
+        log.info(longestPalindrome("a"));
+        log.info(longestPalindrome("bb"));
+
     }
 }
+/**
+    private static String longestPalindrome(String s) {
+        int resLen = 0, n = s.length();
+        String ans = "";
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                int l = i, r = j;
+                while (l < r && s.charAt(l) == s.charAt(r)) {
+                    ++l; --r;
+                }
+                if (l >= r && resLen < (j - i + 1)) {
+                    resLen = j - i + 1;
+                    ans = s.substring(i, j + 1);
+                }
+            }
+        }
+        return ans;
+    }
+*/
+/**
+    private static boolean isPal(String s) {
+        StringBuilder str = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; --i) {
+            str.append(s.charAt(i));
+        }
+        log.info("str="+str);
+        String newS = str.toString();
+        return newS.equals(s);
+    }
+    private static String longestPalindrome(String s) {
+        int n = s.length();
+        if (s.length() < 2) return s;
+        int maxLen = 0;
+        String ans = "";
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n + 1; ++j) {
+                if (isPal(s.substring(i, j))) {
+                    if (s.substring(i, j).length() > maxLen) {
+                        ans = s.substring(i, j);
+                        maxLen = s.substring(i, j).length();
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+*/
