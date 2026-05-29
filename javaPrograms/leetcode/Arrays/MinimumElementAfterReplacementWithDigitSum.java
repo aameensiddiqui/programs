919356865d4e81bb6c5da3d8fd62f7f6e45ee26c
aameensiddiqui <aameensddq@gmail.com>
2026-05-29 10:14:12 +0530

    leetcode: minimum element after replacement with digit sum

---
+++ javaPrograms/leetcode/Arrays/MinimumElementAfterReplacementWithDigitSum.java
@@ -0,0 +1,49 @@
+/**
+   3300. Minimum Element After Replacement With Digit Sum
+
+   You are given an integer array nums.
+   You replace each element in nums with the sum of its digits.
+   Return the minimum element in nums after all replacements.
+
+   Example 1:
+   Input: nums = [10,12,13,14]
+   Output: 1
+   Explanation:
+   nums becomes [1, 3, 4, 5] after all replacements, with minimum element 1.
+
+   Example 2:
+   Input: nums = [1,2,3,4]
+   Output: 1
+   Explanation:
+   nums becomes [1, 2, 3, 4] after all replacements, with minimum element 1.
+
+   Example 3:
+   Input: nums = [999,19,199]
+   Output: 10
+   Explanation:
+   nums becomes [27, 10, 19] after all replacements, with minimum element 10.
+
+   Constraints:
+   1 <= nums.length <= 100
+   1 <= nums[i] <= 104
+*/
+class MinimumElementAfterReplacementWithDigitSum {
+    private static int minElement(int[] nums) {
+        int m = Integer.MAX_VALUE;
+        for (int n : nums) {
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit;
+                n /= 10;
+            }
+            m = Math.min(m, sum);
+        }
+        return m;
+    }
+    public static void main(String[] args) {
+        Log.info(minElement(new int[] {10,12,13,14}));
+        Log.info(minElement(new int[] {1,2,3,4}));
+        Log.info(minElement(new int[] {999,19,199}));
+    }
+}
