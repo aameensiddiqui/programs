5a7e20626ccdafd786d8badefed46691a4c0445a
aameensiddiqui <aameensddq@gmail.com>
2026-06-08 12:43:24 +0530

    leetcode: partition array according to given pivot

---
+++ javaPrograms/leetcode/SlidingWindowTwoPointer/PartitionArrayAccordingToGivenPivot.java
@@ -0,0 +1,77 @@
+/**
+   2161. Partition Array According to Given Pivot
+
+   You are given a 0-indexed integer array nums and an integer pivot.
+   Rearrange nums such that the following conditions are satisfied:
+
+   Every element less than pivot appears before every element greater than pivot.
+   Every element equal to pivot appears in between the elements less than and greater than pivot.
+   The relative order of the elements less than pivot and the elements greater than pivot is maintained.
+   More formally, consider every pi, pj where pi is the new position of the ith element and pj is
+   the new position of the jth element. If i < j and both elements are smaller (or larger) than pivot,
+   then pi < pj.
+   Return nums after the rearrangement.
+
+   Example 1:
+   Input: nums = [9,12,5,10,14,3,10], pivot = 10
+   Output: [9,5,3,10,10,12,14]
+   Explanation:
+   The elements 9, 5, and 3 are less than the pivot so they are on the left side of the array.
+   The elements 12 and 14 are greater than the pivot so they are on the right side of the array.
+   The relative ordering of the elements less than and greater than pivot is also maintained. [9, 5, 3]
+   and [12, 14] are the respective orderings.
+
+   Example 2:
+   Input: nums = [-3,4,3,2], pivot = 2
+   Output: [-3,2,4,3]
+   Explanation:
+   The element -3 is less than the pivot so it is on the left side of the array.
+   The elements 4 and 3 are greater than the pivot so they are on the right side of the array.
+   The relative ordering of the elements less than and greater than pivot is also maintained. [-3] and
+   [4, 3] are the respective orderings.
+
+   Constraints:
+   1 <= nums.length <= 105
+   -106 <= nums[i] <= 106
+   pivot equals to an element of nums.
+ */
+import java.util.List;
+import java.util.ArrayList;
+
+class PartitionArrayAccordingToGivenPivot {
+    private static int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int l = 0, r = n - 1;
+        int[] ans = new int[n];
+        for (int i = 0, j = n - 1; i < n && j >= 0; ++i, --j) {
+            if (nums[i] < pivot) ans[l++] = nums[i];
+            if (nums[j] > pivot) ans[r--] = nums[j];
+        }
+        while (l <= r) ans[l++] = pivot;
+        return ans;
+    }
+    public static void main(String[] args) {
+        Log.info(pivotArray(new int[] {9,12,5,10,14,3,10}, 10));
+        Log.info(pivotArray(new int[] {-3,4,3,2}, 2));
+    }
+}
+/**
+    private static int[] pivotArray(int[] nums, int pivot) {
+        List<Integer> lt = new ArrayList<>();
+        List<Integer> eq = new ArrayList<>();
+        List<Integer> gt = new ArrayList<>();
+        for (int num : nums) {
+            if (num < pivot) lt.add(num);
+            else if (num == pivot) eq.add(num);
+            else gt.add(num);
+        }
+        int j = 0, n = nums.length;
+        for (int i = 0; i < lt.size() && j < n; ++i, ++j)
+            nums[j] = lt.get(i);
+        for (int i = 0; i < eq.size() && j < n; ++i, ++j)
+            nums[j] = eq.get(i);
+        for (int i = 0; i < gt.size() && j < n; ++i, ++j)
+            nums[j] = gt.get(i);
+        return nums;
+    }
+*/
