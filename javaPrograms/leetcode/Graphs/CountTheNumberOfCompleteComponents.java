63c2dfdd48a9cffdfa3c607546365c8889cfbcb7
aameensiddiqui <aameensddq@gmail.com>
2026-07-11 13:40:46 +0530

    leetcode: count the number of complete components

---
+++ javaPrograms/leetcode/Graphs/CountTheNumberOfCompleteComponents.java
@@ -0,0 +1,78 @@
+/**
+   2685. Count the Number of Complete Components
+
+   You are given an integer n. There is an undirected graph with n vertices, numbered from 0 to n - 1.
+   You are given a 2D integer array edges where edges[i] = [ai, bi] denotes that there exists an
+   undirected edge connecting vertices ai and bi.
+
+   Return the number of complete connected components of the graph.
+
+   A connected component is a subgraph of a graph in which there exists a path between any two vertices,
+   and no vertex of the subgraph shares an edge with a vertex outside of the subgraph.
+
+   A connected component is said to be complete if there exists an edge between every pair of its vertices.
+
+   Example 1:
+   Input: n = 6, edges = [[0,1],[0,2],[1,2],[3,4]]
+   Output: 3
+   Explanation: From the picture above, one can see that all of the components of this graph are complete.
+
+   Example 2:
+   Input: n = 6, edges = [[0,1],[0,2],[1,2],[3,4],[3,5]]
+   Output: 1
+   Explanation: The component containing vertices 0, 1, and 2 is complete since there is an edge between
+   every pair of two vertices. On the other hand, the component containing vertices 3, 4, and 5 is not
+   complete since there is no edge between vertices 4 and 5. Thus, the number of complete components in
+   this graph is 1.
+
+   Constraints:
+   1 <= n <= 50
+   0 <= edges.length <= n * (n - 1) / 2
+   edges[i].length == 2
+   0 <= ai, bi <= n - 1
+   ai != bi
+   There are no repeated edges.
+*/
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class CountTheNumberOfCompleteComponents {
+    private static int E, V;
+    private static void dfs(int x, List<Integer>[] A, boolean[] vis) {
+        V++;
+        E += A[x].size();
+        vis[x] = true;
+        for (int state : A[x]) {
+            if (!vis[state]) dfs(state, A, vis);
+        }
+    }
+    private static int countCompleteComponents(int n, int[][] edges) {
+        List<Integer>[] A = new ArrayList[n];
+        Arrays.setAll(A, a -> new ArrayList<>());
+        boolean[] vis = new boolean[n];
+        int res = 0;
+
+        for (int[] e : edges) {
+            A[e[0]].add(e[1]);
+            A[e[1]].add(e[0]);
+        }
+        // log.info("--------------------------------");
+        // log.info((Object) A);
+        // log.info("--------------------------------");
+
+        for (int i = 0; i < n; ++i) {
+            if (!vis[i]) {
+                E = 0; V = 0;
+                dfs(i, A, vis);
+                if (E == V * (V - 1)) res++;
+            }
+        }
+
+        return res;
+    }
+    public static void main(String[] args) {
+        log.info(countCompleteComponents(6, new int[][] {{0,1},{0,2},{1,2},{3,4}}));
+        log.info(countCompleteComponents(6, new int[][] {{0,1},{0,2},{1,2},{3,4},{3,5}}));
+    }
+}
