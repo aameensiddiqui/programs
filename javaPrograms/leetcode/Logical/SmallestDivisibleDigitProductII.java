6eecdd2194ea7b3a44b18d2deb8e36f579317eb5
aameensiddiqui <aameensddq@gmail.com>
2026-08-10 13:13:33 +0530

    leetcode: smallest divisible digit product ii

---
+++ javaPrograms/leetcode/Logical/SmallestDivisibleDigitProductII.java
@@ -0,0 +1,10 @@
+class SmallestDivisibleDigitProductII {
+    private static String smallestNumber(String num, int t) {
+        int ans = 0;
+    }
+    public static void main(String[] args) {
+        Log.info("1234", 256);
+        //Log.info("12355", 50);
+        //Log.info("11111", 26);
+    }
+}
